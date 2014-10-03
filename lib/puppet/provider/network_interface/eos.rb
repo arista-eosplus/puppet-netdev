7dbcdbbe9f7980363752c6c7036d7a3910a786d5
Jeff McCune <jeff@puppetlabs.com>
2014-10-03 15:44:50 -0700

    (maint) Move convert_speed from network_interface to EosProviderMethods module
    Without this patch the port_channel provider cannot easily convert speed
    values to and from the catalog values.  This patch addresses the problem
    by moving the convert_speed() method from the network_interface provider
    to the EosProviderMethods module which is mixed into all providers.
    This avoids code duplication while also providing the ability to convert
    speed values in a consistent way across providers.

--- lib/puppet/provider/network_interface/eos.rb
+++ lib/puppet/provider/network_interface/eos.rb
@@ -126,35 +126,4 @@ Puppet::Type.type(:network_interface).provide(:eos) do
 
     api.set_interface_state(resource[:name], arg)
   end
-
-  ##
-  # convert_speed takes a speed value from the catalog as a string and converts
-  # it to a speed prefix suitable for the Arista API.  The following table is
-  # used to perform the conversion.
-  #
-  #   10000full  Disable autoneg and force 10 Gbps/full duplex operation
-  #   1000full   Disable autoneg and force 1 Gbps/full duplex operation
-  #   1000half   Disable autoneg and force 1 Gbps/half duplex operation
-  #   100full    Disable autoneg and force 100 Mbps/full duplex operation
-  #   100gfull   Disable autoneg and force 100 Gbps/full duplex operation
-  #   100half    Disable autoneg and force 100 Mbps/half duplex operation
-  #   10full     Disable autoneg and force 10 Mbps/full duplex operation
-  #   10half     Disable autoneg and force 10 Mbps/half duplex operation
-  #   40gfull    Disable autoneg and force 40 Gbps/full duplex operation
-  #
-  # @param [String] speed The speed specified in the catalog, e.g. 1g
-  #
-  # @api private
-  #
-  # @return [String] The speed for the API, e.g. 1000
-  def convert_speed(value)
-    speed = value.to_s
-    if /g$/i.match(speed) && (speed.to_i > 40)
-      speed
-    elsif /g$/i.match(speed)
-      (speed.to_i * 1000).to_s
-    elsif /m$/i.match(speed)
-      speed.to_i.to_s
-    end
-  end
 end
