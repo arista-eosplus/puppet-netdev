192384160b391de5c30530354d14bdee5343c147
Peter Sprygada <sprygada@gmail.com>
2014-11-05 21:11:37 -0500

    initial add of ntp_config provider

---
+++ lib/puppet/provider/ntp_config/eos.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+require 'puppet/type'
+require 'puppet_x/eos/provider'
+
+Puppet::Type.type(:ntp_config).provide(:eos) do
+
+  # Create methods that set the @property_hash for the #flush method
+  mk_resource_methods
+
+  # Mix in the api as instance methods
+  include PuppetX::Eos::EapiProviderMixin
+
+  # Mix in the api as class methods
+  extend PuppetX::Eos::EapiProviderMixin
+
+  def initialize(resource = {})
+    super(resource)
+    @property_flush = {}
+  end
+
+  def self.instances
+    result = eapi.Ntp.get
+    provider_hash = { name: 'configuration', ensure: :present }
+    provider_hash[:source_interface] = result['source_interface']
+    [new(provider_hash)]
+  end
+
+  def source_interface=(val)
+    eapi.Ntp.set_source_interface(value: val)
+    @property_hash[:source_interface] = val
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+end
