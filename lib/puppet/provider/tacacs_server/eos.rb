21f180d291288c8e2dd7a05f2101a860d07c3e9a
Jeff McCune <jeff@puppetlabs.com>
2014-11-10 10:03:34 -0800

    (ARISTA-32) Create & destroy tacacs_server resources
    Without this patch the tacacs_server provider does not have the ability
    to create or destroy resources on the target device.
    
    This patch addresses the problem by implementing the property setter
    methods and deferring to the flush method to create the resource on the
    target device.  This patch also implements the provider create and
    destroy methods.

--- lib/puppet/provider/tacacs_server/eos.rb
+++ lib/puppet/provider/tacacs_server/eos.rb
@@ -16,8 +16,14 @@ Puppet::Type.type(:tacacs_server).provide(:eos) do
     api = eapi.Tacacs
     servers = api.servers
     servers.map do |api_attributes|
-      puppet_attributes = { name: namevar(api_attributes), ensure: :present, }
-      new(api_attributes.merge(puppet_attributes))
+      puppet_attributes = { name: namevar(api_attributes), ensure: :present }
+      single_connection = api_attributes[:multiplex] ? :true : :false
+      puppet_attributes[:single_connection] = single_connection
+      # Filter out API attributes e.g. :multiplex => :single_connection
+      resource_attributes = api_attributes.select do |key, _|
+        resource_type.allattrs.include?(key)
+      end
+      new(resource_attributes.merge(puppet_attributes))
     end
   end
 
@@ -29,6 +35,70 @@ Puppet::Type.type(:tacacs_server).provide(:eos) do
   def exists?
     @property_hash[:ensure] == :present
   end
+
+  def create
+    @property_flush = resource.to_hash
+  end
+
+  def destroy
+    @property_flush = resource.to_hash
+  end
+
+  def hostname=(value)
+    @property_flush[:hostname] = value
+  end
+
+  def port=(value)
+    @property_flush[:port] = value
+  end
+
+  def single_connection=(value)
+    @property_flush[:single_connection] = value
+  end
+
+  def timeout=(value)
+    @property_flush[:timeout] = value
+  end
+
+  def key=(value)
+    @property_flush[:key] = value
+  end
+
+  def key_format=(value)
+    @property_flush[:key_format] = value
+  end
+
+  def flush
+    api = eapi.Tacacs
+    desired_state = @property_hash.merge(@property_flush)
+    # Handle :single_connection => :multiplex mapping
+    multiplex = desired_state.delete(:single_connection)
+    if multiplex == :true
+      desired_state[:multiplex] = true
+    else
+      desired_state[:multiplex] = false
+    end
+    validate_identity(desired_state)
+    case desired_state[:ensure]
+    when :present
+      api.update_server(desired_state)
+    when :absent
+      api.remove_server(desired_state)
+    end
+    @property_hash.merge!(@property_flush)
+  end
+
+  ##
+  # validate_identity checks to make sure there are enough options specified to
+  # uniquely identify a radius server resource.
+  def validate_identity(opts = {})
+    errors = false
+    missing = [:hostname, :port].reject { |k| opts[k] }
+    errors = !missing.empty?
+    msg = "Invalid options #{opts.inspect} missing: #{missing.join(', ')}"
+    fail Puppet::Error, msg if errors
+  end
+  private :validate_identity
 
   def self.namevar(opts)
     hostname  = opts[:hostname] or fail ArgumentError, 'hostname required'
--- spec/unit/puppet/provider/tacacs_server/eos_spec.rb
+++ spec/unit/puppet/provider/tacacs_server/eos_spec.rb
@@ -49,6 +49,64 @@ describe Puppet::Type.type(:tacacs_server).provider(:eos) do
       it 'sets the name parameter as <hostname>/<auth_port>/<acct_port>' do
         subject.each { |i| expect(i.name).to match /^.*?\/\d+$/ }
       end
+      it 'one instance, 1.2.3.4/4949 has single_connection == :true' do
+        instance = subject.find { |i| i.single_connection == :true }
+        expect(instance).not_to be_nil
+        expect(instance.hostname).to eq '1.2.3.4'
+        expect(instance.port).to eq 4949
+      end
+    end
+  end
+
+  describe 'instance methods' do
+    before :each do
+      allow(provider.eapi).to receive(:Tacacs).and_return(eapi)
+    end
+
+    describe '#flush' do
+      context 'after create' do
+        subject do
+          provider.create
+          provider.flush
+        end
+
+        it 'calls PuppetX::Eos::Tacacs#update_server' do
+          expect(eapi).to receive(:update_server)
+            .with(include(hostname: '127.0.0.1'))
+            .and_return(true)
+          subject
+        end
+
+        context 'when single_connection is :true' do
+          let(:resource_override) do
+            { single_connection: :true }
+          end
+          it 'calls PuppetX::Eos::Tacacs#update_server with multiplex: true' do
+            expect(eapi).to receive(:update_server)
+              .with(include(multiplex: true))
+              .and_return(true)
+            subject
+          end
+        end
+      end
+
+      context 'after destroy' do
+        let :resource_override do
+          { ensure: :absent }
+        end
+
+        subject do
+          provider.destroy
+          provider.flush
+        end
+
+        it 'calls PuppetX::Eos::Tacacs#remove_server' do
+          expect(eapi).to receive(:remove_server)
+            .with(include(hostname: '127.0.0.1'))
+            .and_return(true)
+          subject
+        end
+      end
     end
   end
 end
