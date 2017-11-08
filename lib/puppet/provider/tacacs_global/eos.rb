10e13a8c6d7a965d413312a6895a45aeedc7237d
Jere Julian <jere@arista.com>
2017-11-08 16:20:10 -0500

    Add support for managing tacacs source-interface per vrf

--- lib/puppet/provider/tacacs_global/eos.rb
+++ lib/puppet/provider/tacacs_global/eos.rb
@@ -1,10 +1,8 @@
-# encoding: utf-8
-
 require 'puppet/type'
 
 begin
   require 'puppet_x/net_dev/eos_api'
-rescue LoadError => detail
+rescue LoadError
   require 'pathname' # JJM WORK_AROUND #14073
   module_base = Pathname.new(__FILE__).dirname
   require module_base + '../../../' + 'puppet_x/net_dev/eos_api'
@@ -27,6 +25,14 @@ Puppet::Type.type(:tacacs_global).provide(:eos) do
     resource = node.api('tacacs').get
     provider_hash = { name: 'settings' }
     provider_hash.merge!(resource[:global])
+    vrfs = []
+    intfs = []
+    resource[:global][:source_interface].each do |vrf, intf|
+      vrfs << vrf
+      intfs << intf
+    end
+    provider_hash[:vrf] = vrfs
+    provider_hash[:source_interface] = intfs
     [new(provider_hash)]
   end
 
@@ -51,6 +57,11 @@ Puppet::Type.type(:tacacs_global).provide(:eos) do
     @property_flush[:key_format] = value
   end
 
+  def source_interface=(value)
+    @flush_source_interface = true
+    @property_flush[:source_interface] = value
+  end
+
   def timeout=(value)
     @flush_timeout = true
     @property_flush[:timeout] = value
@@ -60,13 +71,39 @@ Puppet::Type.type(:tacacs_global).provide(:eos) do
     not_supported 'retransmit_count'
   end
 
+  def vrf=(value)
+    @flush_vrf = true
+    @property_flush[:vrf] = value
+  end
+
+  def source_interface=(value)
+    @flush_source_interface = true
+    @property_flush[:source_interface] = value
+  end
+
+  def vrf=(value)
+    @flush_source_interface = true
+    @property_flush[:vrf] = value
+  end
+
   def flush
     api = node.api('tacacs')
     opts = @property_hash.merge(@property_flush)
-    api.set_global_key(value: opts[:key],
-                       key_format: opts[:key_format]) if @flush_key
+    set_source_interface if @flush_source_interface == true
+    if @flush_key
+      api.set_global_key(value: opts[:key],
+                         key_format: opts[:key_format])
+    end
     api.set_global_timeout(value: opts[:timeout]) if @flush_timeout
     # Update the state in the model to reflect the flushed changes
     @property_hash.merge!(@property_flush)
+  end
+
+  def set_source_interface
+    api = node.api('tacacs')
+    vrfs = @property_flush[:vrf] || @resource[:vrf]
+    ints = @property_flush[:source_interface] || @resource[:source_interface]
+    srcs = Hash[vrfs.zip(ints)]
+    api.set_source_interface(srcs)
   end
 end
